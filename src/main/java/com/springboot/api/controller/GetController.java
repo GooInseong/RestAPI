8073b305c34b85939b30bfc8938502719ed92059
Inseong <gooinsung96@gmail.com>
2022-10-30 17:03:47 +0900

    Get Method

---
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,63 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    @RequestMapping(value ="/hello",method = RequestMethod.GET)
+    public String getHello(){
+        return "HelloWorld";
+    }
+
+    @GetMapping(value="/name")
+    public String getName(){    /*매개 변수가 없는 GET 메서드*/
+        return"Flature";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+        /*매개 변수가 있는 GET 메서드.
+        GetMapping 어노테이션값으로 url 을 입력 할 때 중괄호를 활용하여 어느 위치에서 값을 받을지 표시.
+        또, GetMapping 에 명시된 변수의 이름과 PathVariable 의 변수 명이 같아야함.*/
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+        /*만약  GetMapping 의 변수명과 PathVariable 의 변수명을 동일하게 맞추기 어렵다면
+        * PathVariable에 ()를 열어 변수명을 지정해야 한다.*/
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String name,
+                                   @RequestParam String email,
+                                   @RequestParam String organization   ){
+        return name+" "+email+ " "+organization;
+        /*GET 요청을 구할때, 위처럼 url 경로상에 값을 담아 보내는 방법 외에도,
+        * 쿼리 형식으로 값을 전달 할 수 있음. 경로 뒤에 ?와 {키}={값}&.. 의 형태로
+        * 구성된 요청을 전송 할 수 있다. GetMapping 매소드 매개변수에 @RequestParam
+        * 어노테이션을 사용하고 쿼리의 키값과 동일한 변수를 지정하면 된다.*/
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String,String> param){
+        StringBuilder sb= new StringBuilder();
+        param.entrySet().forEach(map->{
+            sb.append(map.getKey()+" : "+map.getValue()+"\n");
+        });
+        return sb.toString();
+        /*만약 쿼리스트링에 어떤 값이 들어올지 모른다면 Map 객체를 활용하여 값을 받을 수 있다.*/
+    }
+
+    @GetMapping(value = "/request3")
+    public String getRequestParam3(MemberDto memberDto){
+        return memberDto.toString();
+    }
+
+}
